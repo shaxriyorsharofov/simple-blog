9331ebf08bdef7644c248f1942e2213c96e4598a
Shakhriyor <78806700+shaxriyorsharofov@users.noreply.github.com>
2025-05-07 17:26:13 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
