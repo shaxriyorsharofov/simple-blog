2899ab5032b1700daae6822f129acb96b18ae3eb
Shakhriyor <78806700+shaxriyorsharofov@users.noreply.github.com>
2025-05-07 17:25:26 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['164.92.168.68']
+ALLOWED_HOSTS = ['164.92.168.68', 'shaxriyorpy.uz', '127.0.0.1:8000']
 
 
 # Application definition
